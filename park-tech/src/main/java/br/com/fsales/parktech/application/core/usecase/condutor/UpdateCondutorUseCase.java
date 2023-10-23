f6c22034537011ea543a49c15f067887ca2a653a
Fábio Sales <fabio.oliveira.sales@gmail.com>
2023-10-22 22:44:10 -0300

    implementação do metodo de cadastrar condutor.#1

--- park-tech/src/main/java/br/com/fsales/parktech/application/core/usecase/condutor/UpdateCondutorUseCase.java
+++ park-tech/src/main/java/br/com/fsales/parktech/application/core/usecase/condutor/UpdateCondutorUseCase.java
@@ -2,6 +2,7 @@ package br.com.fsales.parktech.application.core.usecase.condutor;
 
 import br.com.fsales.parktech.application.core.domain.Condutor;
 import br.com.fsales.parktech.application.ports.in.UpdateCondutorInputPort;
+import br.com.fsales.parktech.application.ports.out.ConsultarEnderecoPorCepOutputPort;
 import br.com.fsales.parktech.application.ports.out.FindCondutorByIdOutputPort;
 import br.com.fsales.parktech.application.ports.out.UpdateCondutorOutputPort;
 
@@ -11,16 +12,27 @@ public class UpdateCondutorUseCase implements UpdateCondutorInputPort {
 
 	private final FindCondutorByIdOutputPort findCondutorByIdOutputPort;
 
+	private final ConsultarEnderecoPorCepOutputPort consultarEnderecoPorCepOutputPort;
+
 	public UpdateCondutorUseCase(UpdateCondutorOutputPort updateCondutorOutputPort,
-			FindCondutorByIdOutputPort findCondutorByIdOutputPort) {
+			FindCondutorByIdOutputPort findCondutorByIdOutputPort,
+			ConsultarEnderecoPorCepOutputPort consultarEnderecoPorCepOutputPort) {
 		this.updateCondutorOutputPort = updateCondutorOutputPort;
 		this.findCondutorByIdOutputPort = findCondutorByIdOutputPort;
+		this.consultarEnderecoPorCepOutputPort = consultarEnderecoPorCepOutputPort;
 	}
 
 	@Override
 	public Condutor update(Condutor condutor) {
 		findCondutorByIdOutputPort.find(condutor.id())
 			.orElseThrow(() -> new RuntimeException("Condutor não encontrado."));
+
+		var endereco = consultarEnderecoPorCepOutputPort.consultaPorCep(condutor.endereco().cep());
+		if (endereco == null)
+			throw new IllegalArgumentException("CEP inexistente.");
+
+		condutor.endereco().setCidade(endereco.cidade()).setUf(endereco.uf());
+
 		return updateCondutorOutputPort.update(condutor);
 	}
 
--- park-tech/src/main/java/br/com/fsales/parktech/config/condutor/UpdateCondutorConfig.java
+++ park-tech/src/main/java/br/com/fsales/parktech/config/condutor/UpdateCondutorConfig.java
@@ -1,6 +1,7 @@
 package br.com.fsales.parktech.config.condutor;
 
 import br.com.fsales.parktech.application.core.usecase.condutor.UpdateCondutorUseCase;
+import br.com.fsales.parktech.application.ports.out.ConsultarEnderecoPorCepOutputPort;
 import br.com.fsales.parktech.application.ports.out.FindCondutorByIdOutputPort;
 import br.com.fsales.parktech.application.ports.out.UpdateCondutorOutputPort;
 import org.springframework.context.annotation.Bean;
@@ -11,8 +12,10 @@ public class UpdateCondutorConfig {
 
 	@Bean
 	public UpdateCondutorUseCase updateCondutorUseCase(UpdateCondutorOutputPort updateCondutorOutputPort,
-			FindCondutorByIdOutputPort findCondutorByIdOutputPort) {
-		return new UpdateCondutorUseCase(updateCondutorOutputPort, findCondutorByIdOutputPort);
+			FindCondutorByIdOutputPort findCondutorByIdOutputPort,
+			ConsultarEnderecoPorCepOutputPort consultarEnderecoPorCepOutputPort) {
+		return new UpdateCondutorUseCase(updateCondutorOutputPort, findCondutorByIdOutputPort,
+				consultarEnderecoPorCepOutputPort);
 	}
 
 }
