9fce770ca82709511f89eafd4e910a567f0bde32
Fábio Sales <fabio.oliveira.sales@gmail.com>
2023-10-30 19:32:48 -0300

    ajuste no nome do arquivo

---
+++ park-tech/src/test/java/br/com/fsales/parktech/TestMongoConfig.java
@@ -0,0 +1,22 @@
+package br.com.fsales.parktech;
+
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class TestMongoConfig {
+
+	@Bean
+	public MongoTemplate mongoTemplate() {
+		// Crie um mock do MongoTemplate usando o Mockito
+		MongoTemplate mongoTemplate = Mockito.mock(MongoTemplate.class);
+
+		// Especifique o comportamento do mock, se necessário
+		// Mockito.when(mongoTemplate.find(...)).thenReturn(...);
+
+		return mongoTemplate;
+	}
+
+}
