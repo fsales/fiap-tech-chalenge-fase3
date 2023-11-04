c127b725362049e2e3b2a289f7004141f322d76b
Fábio Sales <fabio.oliveira.sales@gmail.com>
2023-11-03 23:43:22 -0300

    Release/0.0.2 (#29)
    * implementação do cadastro de condutor e veiculo.#1
    * implementação do Controle de Tempo Estacionado #3
    * criando arquivo do github action para deploy na aws utilizando o terraform

--- park-tech/src/test/java/br/com/fsales/parktech/ParkTechApplicationTests.java
+++ park-tech/src/test/java/br/com/fsales/parktech/ParkTechApplicationTests.java
@@ -1,10 +1,17 @@
 package br.com.fsales.parktech;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = TestMongoConfig.class)
 class ParkTechApplicationTests {
+
+	@Autowired
+	private MongoTemplate mongoTemplate;
 
 	@Test
 	void contextLoads() {
