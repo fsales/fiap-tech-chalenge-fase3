0fc8b580b57264997166c2e78d5b4ee13b5d6b7d
Fábio Sales <fabio.oliveira.sales@gmail.com>
2023-11-05 17:31:22 -0300

    alteração para criar o ecr e ecs de forma separada

--- terraform/ecr/infra/ecr.tf
+++ terraform/ecr/infra/ecr.tf
@@ -1,7 +1,7 @@
 ### AWS ECR
 resource "aws_ecr_repository" "repositorio_ecr" {
   name = var.nome_repositorio
-
+  force_delete = true  # forçar a exclusão, mesmo que o repositório não esteja vazio
   tags = {
     Terraform     = "true"
     Environment   = "${var.ambiente}"
